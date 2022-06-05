2330796d33b4bdba4b289d231ed2f4a86b3e6014
dtheod <danis.theodoulou.23@gmail.com>
2022-06-06 00:23:32 +0300

    first commit

---
+++ app.R
@@ -0,0 +1,169 @@
+source("deps.R")
+
+shinyApp(
+    ui = dashboardPage(
+        title = "App",
+        fullscreen = TRUE,
+        header = dashboardHeader(
+            title = tags$a(href='https://innoids.net',
+                           tags$img(src = "helios-card.png", height = '60', width = '190')),
+            titleWidth = 200,
+            tags$li(class = "dropdown",
+                    tags$style(".main-header {max-height: 55px}"),
+                    tags$style(".main-header .logo {height: 55px}")
+            ),
+            skin = "light",
+            status = "white",
+            border = TRUE,
+            sidebarIcon = icon("bars"),
+            controlbarIcon = icon("th"),
+            fixed = FALSE,
+            leftUi = tagList(
+                dropdownMenu(
+                    badgeStatus = "info",
+                    type = "notifications",
+                    notificationItem(
+                        inputId = "triggerAction2",
+                        text = "Error!",
+                        status = "danger"
+                    )
+                ),
+                dropdownMenu(
+                    badgeStatus = "info",
+                    type = "tasks",
+                    taskItem(
+                        inputId = "triggerAction3",
+                        text = "My progress",
+                        color = "orange",
+                        value = 10
+                    )
+                )
+            ),
+            rightUi = dropdownMenu(
+                badgeStatus = "danger",
+                type = "messages",
+                messageItem(
+                    inputId = "triggerAction1",
+                    message = "message 1",
+                    from = "Divad Nojnarg",
+                    image = "https://adminlte.io/themes/v3/dist/img/user3-128x128.jpg",
+                    time = "today",
+                    color = "lime"
+                )
+            )
+        ),
+        sidebar = dashboardSidebar(
+            skin = "light",
+            status = "primary",
+            elevation = 3,
+            sidebarMenu(
+                sidebarHeader("Tabs"),
+                menuItem(
+                    "Item 1",
+                    tabName = "item1",
+                    icon = icon("sliders")
+                ),
+                menuItem(
+                    "Item 2",
+                    tabName = "item2",
+                    icon = icon("id-card")
+                )
+            )
+        ),
+        controlbar = dashboardControlbar(
+            skin = "light",
+            pinned = TRUE,
+            collapsed = FALSE,
+            overlay = FALSE,
+            controlbarMenu(
+                id = "controlbarmenu",
+                controlbarItem(
+                    title = "Item 1",
+                    sliderInput(
+                        inputId = "obs",
+                        label = "Number of observations:",
+                        min = 0,
+                        max = 1000,
+                        value = 500
+                    ),
+                    column(
+                        width = 12,
+                        align = "center",
+                        radioButtons(
+                            inputId = "dist",
+                            label = "Distribution type:",
+                            c(
+                                "Normal" = "norm",
+                                "Uniform" = "unif",
+                                "Log-normal" = "lnorm",
+                                "Exponential" = "exp"
+                            )
+                        )
+                    )
+                ),
+                controlbarItem(
+                    "Item 2",
+                    "Simple text"
+                )
+            )
+        ),
+        footer = dashboardFooter(
+            left = a(
+                href = "https://twitter.com/divadnojnarg",
+                target = "_blank", "@The-ICS"
+            ),
+            right = "2022"
+        ),
+        body = dashboardBody(
+            tags$head(
+                tags$link(rel = "stylesheet", type = "text/css", href = "fonts.css")
+            ),
+            tabItems(
+                tabItem(
+                    tabName = "item1",
+                    fluidRow(
+                        lapply(1:3, FUN = function(i) {
+                            sortable(
+                                width = 4,
+                                p(class = "text-center", paste("Column", i)),
+                                lapply(1:2, FUN = function(j) {
+                                    box(
+                                        title = paste0("I am the ", j, "-th card of the ", i, "-th column"),
+                                        width = 12,
+                                        "Click on my header"
+                                    )
+                                })
+                            )
+                        })
+                    )
+                ),
+                tabItem(
+                    tabName = "item2",
+                    box(
+                        title = "Card with messages",
+                        width = 9,
+                        userMessages(
+                            width = 12,
+                            status = "success",
+                            userMessage(
+                                author = "Alexander Pierce",
+                                date = "20 Jan 2:00 pm",
+                                image = "https://adminlte.io/themes/AdminLTE/dist/img/user1-128x128.jpg",
+                                type = "received",
+                                "Is this template really for free? That's unbelievable!"
+                            ),
+                            userMessage(
+                                author = "Dana Pierce",
+                                date = "21 Jan 4:00 pm",
+                                image = "https://adminlte.io/themes/AdminLTE/dist/img/user5-128x128.jpg",
+                                type = "sent",
+                                "Indeed, that's unbelievable!"
+                            )
+                        )
+                    )
+                )
+            )
+        )
+    ),
+    server = function(input, output) {}
+)
